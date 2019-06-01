70cba6d89484ccaba7673c7ad4184c7a7eb4915e
jakkid <jakkid2540@gmail.com>
2019-06-02 03:03:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication/Main.java
@@ -0,0 +1,76 @@
+package com.example.user.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class Main extends AppCompatActivity implements LocationListener  {
+
+    public static double lat;
+    public static double lng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //permission
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission
+                        (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+
+
+        if (locationManager != null) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 10, this);
+        }
+
+    }
+    public void click(View view) {
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("xxxx", "latitude: " + String.valueOf(location.getLatitude()));
+        Log.d("xxxx", "longitude: " + String.valueOf(location.getLongitude()));
+
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+    }
+
+    @Override
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/example/user/myapplication/MapsActivity.java
@@ -0,0 +1,108 @@
+//package com.example.brainwave.maps;
+//
+//import android.Manifest;
+//import android.content.pm.PackageManager;
+//import android.graphics.Bitmap;
+//import android.graphics.Canvas;
+//import android.graphics.Color;
+//import android.graphics.Paint;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v4.app.FragmentActivity;
+//import android.os.Bundle;
+//import android.util.Log;
+//
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
+//
+//import java.util.ArrayList;
+//import java.util.Random;
+//
+//import static java.lang.Math.random;
+//
+//public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//
+//    private GoogleMap mMap;
+//
+//    private ArrayList<Double>  latitude_array = new ArrayList<>();
+//    private ArrayList<Double> longitude_array = new ArrayList<>();
+//    private ArrayList<Integer> db = new ArrayList<>();
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        setContentView(R.layout.activity_maps);
+//        randLocation();
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//    }
+//
+//    private void randLocation(){
+//        for (int i=0; i<1 ; i++ ){
+//            Random r_lat = new Random();
+//            Random r_lng = new Random();
+//            Random r_db = new Random();
+//
+//            latitude_array.add(13.82 + (r_lat.nextFloat()/40));
+//            longitude_array.add(100.51 + (r_lng.nextFloat()/40));
+//            db.add(r_db.nextInt(50)+40);
+//        }
+//    }
+//
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        plotMarker();
+//
+//        double currentLat = MainActivity.lat;
+//        double currentLng = MainActivity.lng;
+//
+//        Log.d("xxxx", "lat: " + String.valueOf(currentLat));
+//        Log.d("xxxx", "lng: " + String.valueOf(currentLng));
+//
+//        LatLng current = new LatLng(currentLat, currentLng);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 14));
+//
+//
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        mMap.setMyLocationEnabled(true);
+//    }
+//
+//    private void plotMarker(){
+//        int size = 400;
+//
+//        for (int i=0; i < latitude_array.size(); i++){
+//            Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+//            Bitmap bmp = Bitmap.createBitmap(size, size, conf);
+//            Canvas canvas = new Canvas(bmp);
+//
+//            Paint color = new Paint();
+//
+//            if (db.get(i)>70){
+//                color.setColor(Color.parseColor("#55FF0000"));
+//            }else if (db.get(i)>60){
+//                color.setColor(Color.parseColor("#55FFFF00"));
+//            }else{
+//                color.setColor(Color.parseColor("#5500FF00"));
+//            }
+//            canvas.drawCircle(size/2, size/2, size/2, color);
+//
+//            LatLng bangsue = new LatLng(latitude_array.get(i), longitude_array.get(i));
+//            mMap.addMarker(new MarkerOptions()
+//                    .position(bangsue))
+//                    .setIcon(BitmapDescriptorFactory.fromBitmap(bmp));
+//        }
+//    }
+//}
